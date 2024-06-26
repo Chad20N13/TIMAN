2e5668d706da51fe2ddb1ff59a218513c39a28e0
Chad <hucan@pingcap.com>
2024-06-26 15:02:36 +0800

    test0626_002

---
+++ pkg/binlog_monitor/view001.go
@@ -0,0 +1,25 @@
+package binlog_monitor
+
+import (
+	"fmt"
+)
+
+func view001() {
+
+	defer fmt.Printf("test defer1")
+	defer fmt.Printf("test defer2")
+	a := "chad001" //define a;
+
+	fmt.Println("define var a: " + a)
+
+	fmt.Println("memory address of a :", &a)
+
+	p := &a //define pointer P , directing to address of a,  实际上这个p里面存放的东西，就是变量a的内存地址信息
+
+	fmt.Println("print value of pointer p:", *p)
+
+	var ptr *string = &a
+	fmt.Println("print value of pointer p:", ptr)
+	fmt.Println("print value of a:", *ptr)
+
+}
