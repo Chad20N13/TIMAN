aad8003f2b52988fbb98b9a52929d29a5f86cd26
Chad <hucan@pingcap.com>
2024-06-28 18:46:18 +0800

    test_chars_0628

--- test0716.go
+++ test0716.go
@@ -17,37 +17,51 @@ func main() {
 	if err != nil {
 		return
 	}
+	stat, err := file.Stat()
+	fmt.Println("file stats：", stat)
+	endCursor := stat.Size()
+	what := stat.Name()
+	fmt.Println("filename：", what)
+	what2 := stat.ModTime()
+	fmt.Println("filename：", what2)
+	var size int64 = 2048
+	fmt.Println(size)
+
+	chars := make([]byte, size)
+	_, err = file.Read(chars)
+	fmt.Println("chars：", chars)
+	fmt.Println("length fo chars : ", len(chars))
+
+	fmt.Println("file endcursor", endCursor)
 	fmt.Println("字节数据：", data)
 	fmt.Println("字符串数据：", string(data))
 	fmt.Println("所获取字节的长度：", count)
 
-	slice001 := make([]int, 10, 100)
-	fmt.Println(slice001)
-	fmt.Println(len(slice001))
-	fmt.Println(cap(slice001))
-	slice001[0] = 10
-	slice001[1] = 11
-	fmt.Println(slice001)
-	fmt.Println(len(slice001))
-	fmt.Println(cap(slice001))
+	// slice001 := make([]int, 10, 100)
+	// fmt.Println(slice001)
+	// fmt.Println(len(slice001))
+	// fmt.Println(cap(slice001))
+	// slice001[0] = 10
+	// slice001[1] = 11
+	// fmt.Println(slice001)
+	// fmt.Println(len(slice001))
+	// fmt.Println(cap(slice001))
 
-	slice002 := []int{1, 2, 3, 4}
-	fmt.Println(slice002)
-	fmt.Println(len(slice002))
-	fmt.Println(cap(slice002))
+	// slice002 := []int{1, 2, 3, 4}
+	// fmt.Println(slice002)
+	// fmt.Println(len(slice002))
+	// fmt.Println(cap(slice002))
 
-	slice003 := []byte("abc")
-	chars := []byte("chad")
-	fmt.Println(slice003)
-	fmt.Println(len(slice003))
-	fmt.Println(cap(slice003))
-	slice003 = append(slice003, []byte("  hello ")...)
+	// slice003 := []byte("abc")
+	// // chars := []byte("chad")
 
-	fmt.Println(slice003)
+	// slice003 = append(slice003, []byte("  hello ")...)
 
-	slice003 = append(chars, slice003...)
+	// fmt.Println(slice003)
 
-	fmt.Println(slice003)
+	// slice003 = append(chars, slice003...)
+
+	// fmt.Println(slice003)
 
 }
 
